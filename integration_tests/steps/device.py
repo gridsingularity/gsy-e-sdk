e377a63e7efab5a83be655151fd035e0fee13b82
Spyros Tzavikas <spyros@gridsingularity.com>
2020-02-05 18:37:54 +0100

    TESTS-4: Added integration tests that validate the pv and load external connection.

---
+++ integration_tests/steps/device.py
@@ -0,0 +1,57 @@
+from behave import given, when, then
+from os import system
+from time import sleep
+from integration_tests.test_load_connection import AutoBidOnLoadDevice
+from integration_tests.test_pv_connection import AutoOfferOnPVDevice
+
+
+@given('redis container is started')
+def step_impl(context):
+    system('docker run -d -p 6379:6379 --name redis.container -h redis.container '
+           '--net integtestnet gsyd3a/d3a:redis-staging')
+
+
+@given('d3a container is started using setup file {setup_file}')
+def step_impl(context, setup_file):
+    sleep(3)
+    system(f'docker run -d --env REDIS_URL=redis://redis.container:6379/ --net integtestnet -t d3a '
+           f'run -t 1s -s 15m --setup {setup_file}')
+
+
+@when('the external client container is started with test_load_connection')
+def step_impl(context):
+    # Wait for d3a to activate all areas
+    sleep(5)
+    # Connects one client to the load device
+    context.load = AutoBidOnLoadDevice('load', autoregister=True, redis_url='redis://localhost:6379/')
+
+
+@when('the external client container is started with test_pv_connection')
+def step_impl(context):
+    # Wait for d3a to activate all areas
+    sleep(5)
+    # Connects one client to the load device
+    context.load = AutoOfferOnPVDevice('pv', autoregister=True, redis_url='redis://localhost:6379/')
+
+
+@then('the external client is connecting to the simulation until finished')
+def step_impl(context):
+    # Infinite loop in order to leave the client running on the background
+    # placing bids and offers on every market cycle.
+    # Should stop if an error occurs or if the simulation has finished
+    counter = 0  # Wait for five minutes at most
+    while context.load.errors == 0 and context.load.status != "finished" and counter < 300:
+        sleep(0.5)
+        counter += 0.5
+
+
+@then('the external client does not report errors')
+def step_impl(context):
+    assert context.load.errors == 0
+
+
+@then('the energy bills of the load report the required energy was bought by the load')
+def step_impl(context):
+    print(context.load.latest_stats["device_stats"]["bills"])
+    print()
+    assert context.load.latest_stats["device_stats"]["bills"]["bought"] == (24 * 4 - 2) * 0.05
---
+++ integration_tests/test_pv_connection.py
@@ -0,0 +1,55 @@
+"""
+Test file for the device client. Depends on d3a test setup file strategy_tests.external_devices
+"""
+import json
+from math import isclose
+from d3a_api_client.redis_device import RedisDeviceClient
+
+
+class AutoOfferOnPVDevice(RedisDeviceClient):
+    def __init__(self, *args, **kwargs):
+        self.errors = 0
+        self.status = "running"
+        self.latest_stats = {}
+        super().__init__(*args, **kwargs)
+
+    def on_market_cycle(self, market_info):
+        try:
+            assert "available_energy_kWh" in market_info
+            if market_info["available_energy_kWh"] > 0.0:
+                # Placing an expensive offer to the market that will not be accepted
+                offer = self.offer_energy(market_info["available_energy_kWh"], 50 * market_info["available_energy_kWh"])
+                offer_info = json.loads(offer["offer"])
+                assert offer_info["price"] == 50 * market_info["available_energy_kWh"]
+                assert offer_info["energy"] == market_info["available_energy_kWh"]
+                # Validate that the bid was placed to the market
+                offer_listing = self.list_offers()
+                listed_offer = next(bid for bid in offer_listing["offer_list"] if bid["id"] == offer_info["id"])
+                assert listed_offer["price"] == listed_offer["price"]
+                assert listed_offer["energy"] == listed_offer["energy"]
+                # Try to delete the bid
+                delete_resp = self.delete_offer(offer_info["id"])
+                assert delete_resp["deleted_offer"] == offer_info["id"]
+                # Validate that the bid was deleted from the market
+                empty_listing = self.list_bids()
+                assert not any(o for o in empty_listing["offer_list"] if o["id"] == offer_info["id"])
+                # Place the bid with a price that will be acceptable for trading
+                offer = self.offer_energy(market_info["available_energy_kWh"], 10 * market_info["available_energy_kWh"])
+                offer_info = json.loads(offer["offer"])
+                assert offer_info["price"] == 10 * market_info["available_energy_kWh"]
+                assert offer_info["energy"] == market_info["available_energy_kWh"]
+
+            stats = self.list_stats()
+            traded_slots = stats["market_stats"]["energy_trade_profile"]["sold_energy"]["pv"]["accumulated"].values()
+            assert isclose(stats["device_stats"]["bills"]["bought"], sum(traded_slots))
+
+            print(market_info["start_time"])
+            if market_info["start_time"][-5:] == "23:45":
+                self.status = "finished"
+            self.latest_stats = stats
+        except AssertionError as e:
+            import traceback
+            print(e)
+            print(traceback.format_exc())
+            self.errors += 1
+            raise e
