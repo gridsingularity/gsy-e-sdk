936b540bbdeed56dc381acbcf1d6675ebf76da26
Spyros Tzavikas <spyros@gridsingularity.com>
2020-03-05 11:59:21 +0100

    D3ASIM-2068: Added fixes from review comments.

--- d3a_api_client/rest_market.py
+++ d3a_api_client/rest_market.py
@@ -2,7 +2,7 @@ import logging
 from d3a_api_client.rest_device import logging_decorator
 from concurrent.futures.thread import ThreadPoolExecutor
 from d3a_api_client.websocket_device import WebsocketMessageReceiver, WebsocketThread
-from d3a_api_client.utils import retrieve_jwt_key_from_server
+from d3a_api_client.utils import retrieve_jwt_key_from_server, post_request, get_request
 
 
 root_logger = logging.getLogger()
@@ -24,13 +24,19 @@ class RestMarketClient:
         self.websocket_thread.start()
         self.callback_thread = ThreadPoolExecutor(max_workers=5)
 
+    def _post_request(self, endpoint_suffix, data):
+        return post_request(f"{self._url_prefix}/{endpoint_suffix}/", data, self.jwt_token)
+
+    def _get_request(self, endpoint_suffix):
+        return get_request(f"{self._url_prefix}/{endpoint_suffix}/", self.jwt_token)
+
     @property
     def _url_prefix(self):
         return f'{self.domain_name}/external-connection/api/{self.simulation_id}/{self.device_id}'
 
     @logging_decorator('market_stats')
     def list_stats(self):
-        if self._post_request('market_stats', {}):
+        if self._get_request('market_stats'):
             return self.dispatcher.wait_for_command_response('market_stats')
 
     @logging_decorator('grid_fees')
