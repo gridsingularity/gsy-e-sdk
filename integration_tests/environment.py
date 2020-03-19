5f416c3c6c52b35110c2b8450ab8ecd4ab4c9d1b
faizan2590 <faizan2590@gmail.com>
2020-03-19 14:30:34 +0100

    D3ASIM-2114: Adding d3a-interface as part of requirements

--- integration_tests/environment.py
+++ integration_tests/environment.py
@@ -2,6 +2,7 @@ from os import system
 
 
 def before_all(context):
+    system(f'docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)')
     system('bash integration_tests/build_test_containers.sh')
     system(f'docker network create integtestnet')
 
