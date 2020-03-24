2116022325ddeee21956099720ddd73333ca16cb
Hannes Diedrich <hannes@gridsingularity.com>
2020-03-24 18:14:09 +0100

    D3ASIM-2158: Added list_dso_market_stats, market_cycle and finish event to market API.

--- d3a_api_client/redis_market.py
+++ d3a_api_client/redis_market.py
@@ -2,6 +2,8 @@ import logging
 import json
 from redis import StrictRedis
 from d3a_interface.utils import wait_until_timeout_blocking
+from concurrent.futures.thread import ThreadPoolExecutor
+from d3a_api_client.constants import MAX_WORKER_THREADS
 
 
 class RedisMarketClient:
@@ -11,6 +13,7 @@ class RedisMarketClient:
         self.pubsub = self.redis_db.pubsub()
         self._subscribe_to_response_channels()
         self._blocking_command_responses = {}
+        self.executor = ThreadPoolExecutor(max_workers=MAX_WORKER_THREADS)
 
     def _generate_command_response_callback(self, command_type):
         def _command_received(msg):
@@ -32,7 +35,11 @@ class RedisMarketClient:
     def _subscribe_to_response_channels(self):
         channel_subs = {f"{self.area_id}/response/{command_name}":
                         self._generate_command_response_callback(command_name)
-                        for command_name in ["market_stats", "grid_fees"]}
+                        for command_name in ["market_stats", "grid_fees", "dso_market_stats"]}
+        channel_subs.update({
+            f'{self.area_id}/market-events/market': self._on_market_cycle,
+            f'{self.area_id}/events/finish': self._on_finish
+        })
         self.pubsub.subscribe(**channel_subs)
         self.pubsub.run_in_thread(daemon=True)
 
@@ -52,3 +59,30 @@ class RedisMarketClient:
         logging.debug(f"Client tries to change grid fees.")
         self.redis_db.publish(f"{self.area_id}/grid_fees", json.dumps({"fee": fee_cents_kwh}))
         return self._wait_and_consume_command_response("grid_fees")
+
+    def list_dso_market_stats(self, market_slot_list):
+        logging.debug(f"Client tries to read dso_market_stats.")
+        self.redis_db.publish(f"{self.area_id}/dso_market_stats", json.dumps({"market_slots": market_slot_list}))
+        return self._wait_and_consume_command_response("dso_market_stats")
+
+    def _on_market_cycle(self, msg):
+        message = json.loads(msg["data"])
+        logging.info(f"A new market was created. Market information: {message}")
+
+        def executor_function():
+            self.on_market_cycle(message)
+        self.executor.submit(executor_function)
+
+    def on_market_cycle(self, market_info):
+        pass
+
+    def _on_finish(self, msg):
+        message = json.loads(msg["data"])
+        logging.info(f"Simulation finished. Information: {message}")
+
+        def executor_function():
+            self.on_finish(message)
+        self.executor.submit(executor_function)
+
+    def on_finish(self, finish_info):
+        pass
