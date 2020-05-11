6dfadde634fddb40794028608bc565bdfa0dc865
Lea Juskiewenski <lea.jski@gmail.com>
2020-05-11 15:56:53 +0200

    TESTS-20: add 10 load and 10 pv instances

--- tests/test_auto_offer_on_device_rest.py
+++ tests/test_auto_offer_on_device_rest.py
@@ -22,7 +22,7 @@ class AutoOfferBidOnMarket(RestDeviceClient):
         :param market_info: Incoming message containing the newly-created market info
         :return: None
         """
-        if self.registered is False:
+        if self.registered is False or self.is_finished is True:
             return
         logging.debug(f"New market information {market_info}")
         if "available_energy_kWh" in market_info["device_info"] and market_info["device_info"]["available_energy_kWh"] > 0.0:
@@ -30,8 +30,8 @@ class AutoOfferBidOnMarket(RestDeviceClient):
             logging.debug(f"Offer placed on the new market: {offer}")
             assert len(self.list_offers()) == 1
         if "energy_requirement_kWh" in market_info["device_info"] and market_info["device_info"]["energy_requirement_kWh"] > 0.0:
-            bid = self.bid_energy(market_info["device_info"]["energy_requirement_kWh"], 30)
-            logging.error(f"Bid placed on the new market: {bid}")
+            bid = self.bid_energy(market_info["device_info"]["energy_requirement_kWh"] / 2, 30)
+            logging.info(f"Bid placed on the new market: {bid}")
             assert len(self.list_bids()) == 1
 
     def on_tick(self, tick_info):
@@ -43,17 +43,30 @@ class AutoOfferBidOnMarket(RestDeviceClient):
     def on_finish(self, finish_info):
         self.is_finished = True
 
-# Connects one client to the load device
-load = AutoOfferBidOnMarket(
-    simulation_id= str(sys.argv[1]), #"2d520066-18f8-4e2d-9781-eae419e39af4",
-    device_id= get_area_uuid_from_area_name_and_collaboration_id(str(sys.argv[1]), str(sys.argv[2]), 'https://d3aweb-dev.gridsingularity.com'),
-    domain_name='https://d3aweb-dev.gridsingularity.com',
-    websockets_domain_name='wss://d3aweb-dev.gridsingularity.com/external-ws',
-    autoregister=True)
-# Connects a second client to the pv device
-# pv = AutoOfferBidOnMarket('pv', autoregister=True)
+number_of_areas = 10
+load_names = [str(sys.argv[2]) if i == 0 else str(sys.argv[2])+' '+str(i+1) for i in range(number_of_areas)]
+pv_names = [str(sys.argv[3]) if i == 0 else str(sys.argv[3])+' '+str(i+1) for i in range(number_of_areas)]
+connected_devices = []
+
+for i in range(number_of_areas):
+    # Connects one client to the load device
+    load = AutoOfferBidOnMarket(
+        simulation_id= str(sys.argv[1]), 
+        device_id= get_area_uuid_from_area_name_and_collaboration_id(str(sys.argv[1]), load_names[i], 'https://d3aweb-dev.gridsingularity.com'),
+        domain_name='https://d3aweb-dev.gridsingularity.com',
+        websockets_domain_name='wss://d3aweb-dev.gridsingularity.com/external-ws',
+        autoregister=True)
+    connected_devices.append(load)
+    # Connects a second client to the pv device
+    pv = AutoOfferBidOnMarket(
+        simulation_id= str(sys.argv[1]), 
+        device_id= get_area_uuid_from_area_name_and_collaboration_id(str(sys.argv[1]), pv_names[i], 'https://d3aweb-dev.gridsingularity.com'),
+        domain_name='https://d3aweb-dev.gridsingularity.com',
+        websockets_domain_name='wss://d3aweb-dev.gridsingularity.com/external-ws',
+        autoregister=True)
+    connected_devices.append(pv)
 
 # Infinite loop in order to leave the client running on the background
 # placing bids and offers on every market cycle.
-while not load.is_finished:
+while not any([connected_device.is_finished for connected_device in connected_devices]):
     sleep(0.5)
