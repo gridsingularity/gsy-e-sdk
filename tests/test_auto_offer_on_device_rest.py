5d3b4f481a32432691428c8b3da474a774679e5f
Lea Juskiewenski <lea.jski@gmail.com>
2020-06-15 16:51:51 +0200

    TESTS-24: parse list of load names and pv names as argument

--- tests/test_auto_offer_on_device_rest.py
+++ tests/test_auto_offer_on_device_rest.py
@@ -4,7 +4,6 @@ Test file for the device client. Depends on d3a test setup file strategy_tests.e
 import logging
 import sys
 import argparse
-import numpy as np
 from time import sleep
 from random import randint
 from d3a_api_client.rest_device import RestDeviceClient
@@ -13,13 +12,10 @@ from d3a_api_client.utils import get_area_uuid_from_area_name_and_collaboration_
 #parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--simulation_id", type=str, help="Simulation uuid")
-parser.add_argument("--load", type=str, help="Load device")
-parser.add_argument("--pv", type=str, help="PV device")
-parser.add_argument("--researcher_id", type=float, help="Researcher id")
+parser.add_argument("--load_names", nargs='+')
+parser.add_argument("--pv_names", nargs='+')
 args = parser.parse_args()
 
-number_of_areas = 10
-number_of_researcher = 2
 connected_devices = []
 
 class AutoOfferBidOnMarket(RestDeviceClient):
@@ -38,7 +34,6 @@ class AutoOfferBidOnMarket(RestDeviceClient):
         """
         if self.registered is False or self.is_finished is True:
             return
-        #sleep(randint(1,10)/100.)
         logging.debug(f"New market information {market_info}")
         if "available_energy_kWh" in market_info["device_info"] and market_info["device_info"]["available_energy_kWh"] > 0.0:
             offer = self.offer_energy_rate(market_info["device_info"]["available_energy_kWh"] / 2, 16)
@@ -58,25 +53,13 @@ class AutoOfferBidOnMarket(RestDeviceClient):
     def on_finish(self, finish_info):
         self.is_finished = True
 
-#create a list with the number of areas
-areas=range(number_of_areas)
-#split the list to have one list for each researcher
-#the list contains lists of the devices numbers each researcher will connect to
-areas_split = [i.tolist() for i in np.array_split(np.array(areas),number_of_researcher)]
-
-if args.researcher_id <= number_of_researcher:
-    #create a list with the names of the Load the researcher connects to 
-    load_names = [args.load if i == 0 else f'{args.load} {i+1}' for i in areas_split[int(args.researcher_id-1)]]
-    #create a list with the names of the PV the researcher connects to 
-    pv_names = [args.pv if i == 0 else f'{args.pv} {i+1}' for i in areas_split[int(args.researcher_id-1)]]
-else:
-    raise ValueError(f'The researcher id is out of range. The number of researcher is {number_of_researcher}.')
-
-for i in range(len(load_names)):
+for i in range(len(args.load_names)):
     # Connects one client to the load device
     load = AutoOfferBidOnMarket(
         simulation_id= args.simulation_id, 
-        device_id= get_area_uuid_from_area_name_and_collaboration_id(args.simulation_id, load_names[i], 'https://d3aweb-dev.gridsingularity.com'),
+        device_id= get_area_uuid_from_area_name_and_collaboration_id(
+            args.simulation_id, args.load_names[i], 
+            'https://d3aweb-dev.gridsingularity.com'),
         domain_name='https://d3aweb-dev.gridsingularity.com',
         websockets_domain_name='wss://d3aweb-dev.gridsingularity.com/external-ws',
         autoregister=True)
@@ -84,7 +67,9 @@ for i in range(len(load_names)):
     # Connects a second client to the pv device
     pv = AutoOfferBidOnMarket(
         simulation_id= args.simulation_id, 
-        device_id= get_area_uuid_from_area_name_and_collaboration_id(args.simulation_id, pv_names[i], 'https://d3aweb-dev.gridsingularity.com'),
+        device_id= get_area_uuid_from_area_name_and_collaboration_id(
+            args.simulation_id, args.pv_names[i], 
+            'https://d3aweb-dev.gridsingularity.com'),
         domain_name='https://d3aweb-dev.gridsingularity.com',
         websockets_domain_name='wss://d3aweb-dev.gridsingularity.com/external-ws',
         autoregister=True)
