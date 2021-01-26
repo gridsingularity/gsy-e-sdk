89ab0f3bca03f2d55e878d85de944e15aa262a45
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-01-26 10:25:39 +0200

    Adapted to reviews

--- d3a_api_client/commands.py
+++ d3a_api_client/commands.py
@@ -8,56 +8,56 @@ class ClientCommandBuffer:
 
     def offer_energy(self, area_uuid, energy, price):
         area_uuid = area_uuid
-        callback_args = {energy, price}
+        args = {energy, price}
         action = Commands.OFFER
-        self.execute(area_uuid, action, callback_args)
+        self.execute(area_uuid, action, args)
 
     def offer_energy_rate(self, area_uuid, energy, rate):
         area_uuid = area_uuid
-        callback_args = {energy, rate}
+        args = {energy, rate}
         action = Commands.OFFER
-        self.execute(area_uuid, action, callback_args)
+        self.execute(area_uuid, action, args)
 
     def bid_energy(self, area_uuid, energy, rate):
         area_uuid = area_uuid
-        callback_args = {energy, rate}
+        args = {energy, rate}
         action = Commands.OFFER
-        self.execute(area_uuid, action, callback_args)
+        self.execute(area_uuid, action, args)
 
     def delete_offer(self, area_uuid, offer_id):
         area_uuid = area_uuid
-        callback_args = {"offer_id": offer_id}
+        args = {"offer_id": offer_id}
         action = Commands.DELETE_OFFER
-        self.execute(area_uuid, action, callback_args)
+        self.execute(area_uuid, action, args)
 
     def delete_bid(self, area_uuid, bid_id):
         area_uuid = area_uuid
-        callback_args = {"bid_id": bid_id}
+        args = {"bid_id": bid_id}
         action = Commands.DELETE_OFFER
-        self.execute(area_uuid, action, callback_args)
+        self.execute(area_uuid, action, args)
 
     def list_offers(self, area_uuid):
         area_uuid = area_uuid
         action = Commands.LIST_OFFERS
-        callback_args = {}
-        self.execute(area_uuid, action, callback_args)
+        args = {}
+        self.execute(area_uuid, action, args)
 
     def list_bids(self, area_uuid):
         area_uuid = area_uuid
         action = Commands.LIST_BIDS
-        callback_args = {}
-        self.execute(area_uuid, action, callback_args)
+        args = {}
+        self.execute(area_uuid, action, args)
 
     def device_info(self, area_uuid):
         area_uuid = area_uuid
         action = Commands.DEVICE_INFO
-        callback_args = {}
-        self.execute(area_uuid, action, callback_args)
+        args = {}
+        self.execute(area_uuid, action, args)
 
-    def execute(self, area_uuid, action, callback_args):
+    def execute(self, area_uuid, action, args):
         if area_uuid and action:
             self._commands_buffer.append(
-                {area_uuid: {"type": command_enum_to_command_name(action), **callback_args}})
+                {area_uuid: {"type": command_enum_to_command_name(action), **args}})
 
     def clear(self):
         self._commands_buffer.clear()
