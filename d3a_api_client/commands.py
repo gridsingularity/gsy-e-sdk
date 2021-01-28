354f54e5012192bd22995cc71dd240addcbe3d83
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-01-28 13:06:46 +0200

    feature/D3ASIM-3213: Adapted to reviews

--- d3a_api_client/commands.py
+++ d3a_api_client/commands.py
@@ -7,54 +7,33 @@ class ClientCommandBuffer:
         self._commands_buffer = []
 
     def offer_energy(self, area_uuid, energy, price):
-        area_uuid = area_uuid
-        args = {energy, price}
-        action = Commands.OFFER
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "price": price})
 
     def offer_energy_rate(self, area_uuid, energy, rate):
-        area_uuid = area_uuid
-        args = {energy, rate}
-        action = Commands.OFFER
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "rate": rate})
 
-    def bid_energy(self, area_uuid, energy, rate):
-        area_uuid = area_uuid
-        args = {energy, rate}
-        action = Commands.OFFER
-        self.execute(area_uuid, action, args)
+    def bid_energy(self, area_uuid, energy, price):
+        self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "price": price})
+
+    def bid_energy_rate(self, area_uuid, energy, rate):
+        self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "rate": rate})
 
     def delete_offer(self, area_uuid, offer_id):
-        area_uuid = area_uuid
-        args = {"offer_id": offer_id}
-        action = Commands.DELETE_OFFER
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.DELETE_OFFER, {"offer_id": offer_id})
 
     def delete_bid(self, area_uuid, bid_id):
-        area_uuid = area_uuid
-        args = {"bid_id": bid_id}
-        action = Commands.DELETE_OFFER
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.DELETE_BID, {"bid_id": bid_id})
 
     def list_offers(self, area_uuid):
-        area_uuid = area_uuid
-        action = Commands.LIST_OFFERS
-        args = {}
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.LIST_OFFERS, {})
 
     def list_bids(self, area_uuid):
-        area_uuid = area_uuid
-        action = Commands.LIST_BIDS
-        args = {}
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.LIST_BIDS, {})
 
     def device_info(self, area_uuid):
-        area_uuid = area_uuid
-        action = Commands.DEVICE_INFO
-        args = {}
-        self.execute(area_uuid, action, args)
+        self._add_to_buffer(area_uuid, Commands.DEVICE_INFO, {})
 
-    def execute(self, area_uuid, action, args):
+    def _add_to_buffer(self, area_uuid, action, args):
         if area_uuid and action:
             self._commands_buffer.append(
                 {area_uuid: {"type": command_enum_to_command_name(action), **args}})
