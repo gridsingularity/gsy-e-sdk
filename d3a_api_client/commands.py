0cb62e8f6c0b963cad37bd5b3af5ea95042e5894
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-02-01 10:33:59 +0200

    D3ASIM-3213: Refactored the commands class

--- d3a_api_client/commands.py
+++ d3a_api_client/commands.py
@@ -12,49 +12,40 @@ class ClientCommandBuffer:
 
     def offer_energy(self, area_uuid, energy, price):
         self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "price": price})
-        return self
 
     def offer_energy_rate(self, area_uuid, energy, rate):
         self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "rate": rate})
-        return self
 
     def bid_energy(self, area_uuid, energy, price):
         self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "price": price})
-        return self
 
     def bid_energy_rate(self, area_uuid, energy, rate):
         self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "rate": rate})
-        return self
 
     def delete_offer(self, area_uuid, offer_id):
         self._add_to_buffer(area_uuid, Commands.DELETE_OFFER, {"offer_id": offer_id})
-        return self
 
     def delete_bid(self, area_uuid, bid_id):
         self._add_to_buffer(area_uuid, Commands.DELETE_BID, {"bid_id": bid_id})
-        return self
 
     def list_offers(self, area_uuid):
         self._add_to_buffer(area_uuid, Commands.LIST_OFFERS, {})
-        return self
 
     def list_bids(self, area_uuid):
         self._add_to_buffer(area_uuid, Commands.LIST_BIDS, {})
-        return self
 
     def device_info(self, area_uuid):
         self._add_to_buffer(area_uuid, Commands.DEVICE_INFO, {})
-        return self
 
     def last_market_stats(self, area_uuid):
         self._add_to_buffer(area_uuid, "last_market_stats", {})
-        return self
 
     def _add_to_buffer(self, area_uuid, action, args):
         if area_uuid and action:
             self._commands_buffer.append(
                 {area_uuid: {"type": command_enum_to_command_name(action)
-                                                        if type(action) == Commands else action, **args}})
+                if type(action) == Commands else action, **args}})
+        return self
 
     def clear(self):
         self._commands_buffer.clear()
