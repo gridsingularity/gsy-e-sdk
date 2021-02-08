22d2a0d309c8d02b47386b847681a273d0c7633e
faizan2590 <faizan2590@gmail.com>
2021-02-08 09:40:10 +0100

    D3ASIM-3218: Accepting absolute,relative to cwd for setup_file and simulation_info.

--- cli.py
+++ cli.py
@@ -60,6 +60,8 @@ _setup_modules = iterate_over_all_modules(modules_path)
 
 
 @main.command()
+@click.option('-b', 'base_setup_path', default=None, type=str,
+              help="Accept absolute or relative path for client script")
 @click.option('--setup', 'setup_module_name', default="auto_offer_bid_on_device",
               help="Setup module of client script. Available modules: [{}]".format(
                   ', '.join(_setup_modules)))
@@ -69,11 +71,12 @@ _setup_modules = iterate_over_all_modules(modules_path)
               type=str, help="D3A domain name")
 @click.option('-w', '--web-socket', default=DEFAULT_WEBSOCKET_DOMAIN,
               type=str, help="D3A websocket URL")
-@click.option('-j', '--json-file-name', type=str,
-              help="JSON Filename containing info related to simulation")
-@click.option('--run-on-redis', is_flag=True, default=False, help="Start the client using the Redis API")
-def run(setup_module_name, username, password, domain_name, web_socket, json_file_name,
-        run_on_redis, **kwargs):
+@click.option('-i', '--simulation-info', type=str,
+              help="Simulation File info (accept absolute and relative path)")
+@click.option('--run-on-redis', is_flag=True, default=False,
+              help="Start the client using the Redis API")
+def run(base_setup_path, setup_module_name, username, password, domain_name, web_socket,
+        simulation_info, run_on_redis, **kwargs):
     if username is not None:
         os.environ["API_CLIENT_USERNAME"] = username
     if password is not None:
@@ -81,14 +84,22 @@ def run(setup_module_name, username, password, domain_name, web_socket, json_fil
     os.environ["API_CLIENT_DOMAIN_NAME"] = domain_name
     os.environ["API_CLIENT_WEBSOCKET_DOMAIN_NAME"] = web_socket
     os.environ["API_CLIENT_RUN_ON_REDIS"] = "true" if run_on_redis else "false"
-    os.environ["JSON_FILE_PATH"] = os.path.join(modules_path[0], json_file_name)
+    if os.path.isabs(simulation_info):
+        os.environ["JSON_FILE_PATH"] = simulation_info
+    else:
+        os.environ["JSON_FILE_PATH"] = os.path.join(os.getcwd(), simulation_info)
 
     try:
-        if SETUP_FILE_PATH:
-            sys.path.append(SETUP_FILE_PATH)
-            importlib.import_module(f"{setup_module_name}")
-        else:
+        if base_setup_path is None:
             importlib.import_module(f"d3a_api_client.setups.{setup_module_name}")
+        elif base_setup_path and os.path.isabs(base_setup_path):
+            sys.path.append(base_setup_path)
+            importlib.import_module(setup_module_name)
+        else:
+            setup_file_path = os.path.join(os.getcwd(), base_setup_path)
+            # print(f"pathhh: {pathhh}")
+            sys.path.append(setup_file_path)
+            importlib.import_module(setup_module_name)
 
     except D3AException as ex:
         raise click.BadOptionUsage(ex.args[0])
