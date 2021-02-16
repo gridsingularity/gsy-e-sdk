8862e1ce3d58dbe81e23754e952fc0c49b2f3e89
Hassan Hammoud <hassan.hammoud754@gmail.com>
2021-02-16 12:36:03 +0200

    Updated energy rates commands

--- d3a_api_client/commands.py
+++ d3a_api_client/commands.py
@@ -17,7 +17,7 @@ class ClientCommandBuffer:
         return self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "price": price})
 
     def offer_energy_rate(self, area_uuid, energy, rate):
-        return self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "rate": rate})
+        return self._add_to_buffer(area_uuid, Commands.OFFER, {"energy": energy, "price": rate * energy})
 
     def update_offer(self, area_uuid, energy, price):
         return self._add_to_buffer(area_uuid, "update_offer", {"energy": energy, "price": price})
@@ -26,7 +26,7 @@ class ClientCommandBuffer:
         return self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "price": price})
 
     def bid_energy_rate(self, area_uuid, energy, rate):
-        return self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "rate": rate})
+        return self._add_to_buffer(area_uuid, Commands.BID, {"energy": energy, "rate": rate * energy})
 
     def update_bid(self, area_uuid, energy, price):
         return self._add_to_buffer(area_uuid, "update_bid", {"energy": energy, "price": price})
