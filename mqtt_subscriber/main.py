caab0fb5b6e8c0fa7aa419a3f1ba8591fe34cb71
faizan2590 <faizan2590@gmail.com>
2021-03-09 13:12:05 +0100

    D3ASIM-3348: Enabling websockets and mqtt consumer to be in a single service.

--- mqtt_subscriber/main.py
+++ mqtt_subscriber/main.py
@@ -2,16 +2,22 @@ import logging
 import traceback
 from mqtt_subscriber import generate_topic_api_client_args_mapping
 from mqtt_subscriber.oli_broker import MQTTConnection
-
+from multiprocessing import Process
+from websocket_subscriber import create_live_data_consumer
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
     topic_api_client_mapping = generate_topic_api_client_args_mapping()
 
     # Connect to the MQTT broker
-    mqtt_connection = MQTTConnection(topic_api_client_mapping)
     try:
-        mqtt_connection.run_forever()
+        p1 = Process(target=create_live_data_consumer())
+        p1.start()
+
+        p2 = Process(target=MQTTConnection(topic_api_client_mapping).run_forever())
+        p2.start()
+
+
     except Exception as e:
         logging.error(f"MQTT Subscriber failed with error {e}")
         logging.error(traceback.format_exc())
