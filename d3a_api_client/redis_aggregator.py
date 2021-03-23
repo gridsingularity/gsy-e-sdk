50a74cc97a3704ee1ef41b971b2ba1c272f11bdf
spyrostz <spyros@gridsingularity.com>
2021-03-23 10:31:13 +0100

    D3ASIM-3203: Adapted cornercase where pubsub thread is reused and channels/patterns with same names are overwritten. Saved callback to a variable and call the already subscribed callback in case the channel is already subscribed to.

--- d3a_api_client/redis_aggregator.py
+++ d3a_api_client/redis_aggregator.py
@@ -31,6 +31,7 @@ class RedisAggregator:
         self._transaction_id_buffer = []
         self._transaction_id_response_buffer = {}
         self.device_uuid_list = []
+        self._subscribed_aggregator_response_cb = None
         self._client_command_buffer = ClientCommandBuffer()
         self._connect_to_simulation(is_blocking=True)
         self._subscribe_to_response_channels()
@@ -43,6 +44,10 @@ class RedisAggregator:
             self.aggregator_uuid = aggr_id
 
     def _subscribe_to_response_channels(self):
+
+        if b'aggregator_response' in self.pubsub.patterns:
+            self._subscribed_aggregator_response_cb = self.pubsub.patterns[b'aggregator_response']
+
         event_channel = f'external-aggregator/*/{self.aggregator_uuid}/events/all'
         channel_dict = {"aggregator_response": self._aggregator_response_callback,
                         event_channel: self._events_callback_dict,
@@ -68,6 +73,8 @@ class RedisAggregator:
         self.executor.submit(executor_function)
 
     def _aggregator_response_callback(self, message):
+        if self._subscribed_aggregator_response_cb is not None:
+            self._subscribed_aggregator_response_cb(message)
         data = json.loads(message['data'])
 
         if data['transaction_id'] in self._transaction_id_buffer:
