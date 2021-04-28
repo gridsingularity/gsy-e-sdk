f939e9cf59ee5474c2d925e5dd8d6b2a1979b036
karthikyadav3 <saikarthikyadav139@gmail.com>
2021-04-28 11:04:43 +0200

    updated changes for market aggregator script

--- d3a_api_client/setups/test_market_aggregator.py
+++ d3a_api_client/setups/test_market_aggregator.py
@@ -5,6 +5,10 @@ from d3a_api_client.utils import get_area_uuid_from_area_name_and_collaboration_
     get_sim_id_and_domain_names
 from d3a_api_client.rest_market import RestMarketClient
 
+# import logging
+logger = logging.getLogger()
+logger.disabled = False
+
 
 class TestAggregator(Aggregator):
 
@@ -21,16 +25,20 @@ class TestAggregator(Aggregator):
         logging.info(f"AGGREGATOR_MARKET_INFO: {market_info}")
         if self.is_finished is True:
             return
-        if "content" not in market_info:
-            return
-        self.fee_cents_per_kwh += 1
+        # if "content" not in market_info:
+        # return
+
+        grid_fee = [1, 2, 3]
+        self.fee_cents_per_kwh += grid_fee[2]
         logging.info(f"{market_info}")
-        for area_event in market_info["content"]:
-            area_uuid = area_event["area_uuid"]
-            if area_uuid is None:
-                continue
-            self.add_to_batch_commands.grid_fees(
-                area_uuid=area_uuid, fee_cents_kwh=self.fee_cents_per_kwh)
+        # for area_event in market_info["content"]:
+        for area_uuid, area_dict in self.latest_grid_tree_flat.items():
+            # area_uuid = area_event["area_uuid"]
+            if area_dict["area_name"] in market_names:
+                if area_uuid is None:
+                    continue
+                self.add_to_batch_commands.grid_fees(
+                    area_uuid=area_uuid, fee_cents_kwh=self.fee_cents_per_kwh)
         response = self.execute_batch_commands()
         logging.info(f"Batch command placed on the new market: {response}")
 
@@ -58,22 +66,16 @@ market_args = {
     "domain_name": domain_name,
     "websockets_domain_name": websockets_domain_name
 }
-
+market_names = ["house 1"]
 house_1_uuid = get_area_uuid_from_area_name_and_collaboration_id(
-    market_args["simulation_id"], "House 1", market_args["domain_name"])
+    market_args["simulation_id"], market_names[0], market_args["domain_name"])
 market_args["area_id"] = house_1_uuid
 house_1 = RestMarketClient(
     **market_args
 )
 
-house_2_uuid = get_area_uuid_from_area_name_and_collaboration_id(
-    market_args["simulation_id"], "House 2", market_args["domain_name"])
-market_args["area_id"] = house_2_uuid
-house_2 = RestMarketClient(
-    **market_args
-)
 house_1.select_aggregator(aggr.aggregator_uuid)
-house_2.select_aggregator(aggr.aggregator_uuid)
+# house_2.select_aggregator(aggr.aggregator_uuid)
 
 while not aggr.is_finished:
     sleep(0.5)
