7ec1b767909ff691d3534cac7d035496b18c46de
Spyros Tzavikas <spyros@gridsingularity.com>
2021-08-19 19:28:53 +0300

    D3ASIM-3549: Added set energy forecast and measurements to the batch commands. Fixed integration tests by pointing to the branch with the d3a fix of bid_energy command.

--- d3a_api_client/enums.py
+++ d3a_api_client/enums.py
@@ -11,6 +11,8 @@ class Commands(Enum):
     DEVICE_INFO = 7
     GRID_FEES = 8
     DSO_MARKET_STATS = 9
+    FORECAST = 10
+    MEASUREMENT = 11
 
 
 command_enum_to_command_name_dict = {
@@ -22,7 +24,9 @@ command_enum_to_command_name_dict = {
     Commands.LIST_BIDS: "list_bids",
     Commands.DEVICE_INFO: "device_info",
     Commands.GRID_FEES: "grid_fees",
-    Commands.DSO_MARKET_STATS: "dso_market_stats"
+    Commands.DSO_MARKET_STATS: "dso_market_stats",
+    Commands.FORECAST: "set_energy_forecast",
+    Commands.MEASUREMENT: "set_energy_measurement"
 }
 
 
