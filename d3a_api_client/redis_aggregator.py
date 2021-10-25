2c7d33e7217699de8614be047f21ad41ab4b124a
Hannes Diedrich <hannes@gridsingularity.com>
2021-10-25 16:48:09 +0200

    D3ASIM-3661: Adapted to pyling warnings.

--- d3a_api_client/redis_aggregator.py
+++ d3a_api_client/redis_aggregator.py
@@ -4,10 +4,11 @@ import uuid
 from concurrent.futures.thread import ThreadPoolExecutor
 from copy import copy
 from threading import Lock
+from typing import Optional, Dict, List
 
-from d3a_interface.utils import wait_until_timeout_blocking, execute_function_util
 from d3a_interface.client_connections.utils import (
     log_market_progression, get_slot_completion_percentage_int_from_message)
+from d3a_interface.utils import wait_until_timeout_blocking, execute_function_util
 from redis import StrictRedis
 
 from d3a_api_client.commands import ClientCommandBuffer
@@ -21,12 +22,14 @@ from d3a_api_client.utils import (
     log_bid_offer_confirmation, log_deleted_bid_offer_confirmation)
 
 
-class RedisAPIException(Exception):
-    pass
+class RedisAggregatorAPIException(Exception):
+    """Exception that is raised in the RedisAggregator."""
 
 
 class RedisAggregator:
+    """Handle aggregator connection via redis to local running simulation."""
 
+    # pylint: disable = too-many-instance-attributes
     def __init__(self, aggregator_name, accept_all_devices=True,
                  redis_url=LOCAL_REDIS_URL):
 
@@ -39,7 +42,6 @@ class RedisAggregator:
         self._transaction_id_buffer = []
         self._transaction_id_response_buffer = {}
         self.device_uuid_list = []
-        self._subscribed_aggregator_response_cb = None
         self._client_command_buffer = ClientCommandBuffer()
 
         self._connect_and_subscribe()
@@ -50,24 +52,24 @@ class RedisAggregator:
         self.latest_grid_tree_flat = {}
         self.area_name_uuid_mapping = {}
 
-    def _connect_and_subscribe(self):
+    def _connect_and_subscribe(self) -> None:
         # order matters here, first connect to the simulation,
         # then subscribe to all other channels that contain the aggregator_uuid
         self._subscribe_to_aggregator_response_and_start_redis_threat()
         self._connect_to_simulation()
         self._subscribe_to_response_channels()
 
-    def _subscribe_to_aggregator_response_and_start_redis_threat(self):
+    def _subscribe_to_aggregator_response_and_start_redis_threat(self) -> None:
         channel_dict = {"aggregator_response": self._aggregator_response_callback}
         self.pubsub.psubscribe(**channel_dict)
         self.pubsub.run_in_thread(daemon=True)
 
-    def _connect_to_simulation(self, is_blocking=True):
+    def _connect_to_simulation(self, is_blocking: bool = True) -> None:
         if self.aggregator_uuid is None:
             aggr_id = self._create_aggregator(is_blocking=is_blocking)
             self.aggregator_uuid = aggr_id
 
-    def _subscribe_to_response_channels(self):
+    def _subscribe_to_response_channels(self) -> None:
         channel_dict = {f"external-aggregator/*/{self.aggregator_uuid}/events/all":
                         self._events_callback_dict,
                         f"external-aggregator/*/{self.aggregator_uuid}/response/batch_commands":
@@ -75,7 +77,8 @@ class RedisAggregator:
                         }
         self.pubsub.psubscribe(**channel_dict)
 
-    def _batch_response(self, message):
+    # pylint: disable = logging-too-many-args
+    def _batch_response(self, message: Dict) -> None:
         logging.debug("AGGREGATORS_BATCH_RESPONSE:: %s", message)
         data = json.loads(message["data"])
         if self.aggregator_uuid != data["aggregator_uuid"]:
@@ -94,9 +97,7 @@ class RedisAggregator:
                         self.area_name_uuid_mapping, asset_uuid))
         self.on_event_or_response(data)
 
-    def _aggregator_response_callback(self, message):
-        if self._subscribed_aggregator_response_cb is not None:
-            self._subscribed_aggregator_response_cb(message)
+    def _aggregator_response_callback(self, message: Dict) -> None:
         data = json.loads(message["data"])
 
         if data["transaction_id"] in self._transaction_id_buffer:
@@ -107,7 +108,7 @@ class RedisAggregator:
         if data["status"] == "UNSELECTED":
             self._unselected_by_device(data)
 
-    def _events_callback_dict(self, message):
+    def _events_callback_dict(self, message: Dict) -> None:
         payload = json.loads(message["data"])
         if payload.get("event") == "market":
             self._on_market_cycle(payload)
@@ -120,10 +121,10 @@ class RedisAggregator:
 
         self._on_event_or_response(payload)
 
-    def _is_transaction_response_received(self, transaction_id):
+    def _is_transaction_response_received(self, transaction_id: str) -> bool:
         return transaction_id not in self._transaction_id_buffer
 
-    def _create_aggregator(self, is_blocking=True):
+    def _create_aggregator(self, is_blocking: bool = True) -> Optional[str]:
         logging.info("Trying to create aggregator %s", self.aggregator_name)
 
         transaction_id = str(uuid.uuid4())
@@ -137,10 +138,12 @@ class RedisAggregator:
                     lambda: self._is_transaction_response_received(transaction_id)
                 )
                 return transaction_id
-            except AssertionError:
-                raise RedisAPIException("API registration process timed out.")
+            except AssertionError as ex:
+                raise RedisAggregatorAPIException("API registration process timed out.") from ex
+        return None
 
-    def delete_aggregator(self, is_blocking=True):
+    def delete_aggregator(self, is_blocking: bool = True) -> None:
+        """Delete aggregator."""
         logging.info("Trying to delete aggregator %s", self.aggregator_name)
 
         transaction_id = str(uuid.uuid4())
@@ -157,19 +160,20 @@ class RedisAggregator:
                     lambda: self._is_transaction_response_received(transaction_id)
                 )
                 return transaction_id
-            except AssertionError:
-                raise RedisAPIException("API has timed out.")
+            except AssertionError as ex:
+                raise RedisAggregatorAPIException("API has timed out.") from ex
+        return None
 
-    def _selected_by_device(self, message):
+    def _selected_by_device(self, message: Dict) -> None:
         if self.accept_all_devices:
             self.device_uuid_list.append(message["device_uuid"])
 
-    def _unselected_by_device(self, message):
+    def _unselected_by_device(self, message: Dict) -> None:
         device_uuid = message["device_uuid"]
         if device_uuid in self.device_uuid_list:
             self.device_uuid_list.remove(device_uuid)
 
-    def _all_uuids_in_selected_device_uuid_list(self, uuid_list):
+    def _all_uuids_in_selected_device_uuid_list(self, uuid_list: List) -> bool:
         for device_uuid in uuid_list:
             if device_uuid not in self.device_uuid_list:
                 logging.error(
@@ -179,23 +183,25 @@ class RedisAggregator:
         return True
 
     @property
-    def add_to_batch_commands(self):
+    def add_to_batch_commands(self) -> ClientCommandBuffer:
         """
-        A property which is meant to be accessed prefixed to a chained function from the ClientCommandBuffer class
+        A property which is meant to be accessed prefixed to a chained function from the
+        ClientCommandBuffer class
         This command will be added to the batch commands buffer
         """
         return self._client_command_buffer
 
     @property
-    def commands_buffer_length(self):
+    def commands_buffer_length(self) -> int:
         """
         Returns the length of the batch commands buffer
         """
         return self._client_command_buffer.buffer_length
 
-    def execute_batch_commands(self, is_blocking=True):
+    def execute_batch_commands(self, is_blocking: bool = True) -> Optional[str]:
+        """Send all buffered batch commands to the simulation."""
         if not self.commands_buffer_length:
-            return
+            return None
         batch_command_dict = self._client_command_buffer.execute_batch()
         self._client_command_buffer.clear()
         self._all_uuids_in_selected_device_uuid_list(batch_command_dict.keys())
@@ -212,10 +218,11 @@ class RedisAggregator:
                     lambda: self._is_transaction_response_received(transaction_id)
                 )
                 return self._transaction_id_response_buffer.get(transaction_id, None)
-            except AssertionError:
-                raise RedisAPIException("API registration process timed out.")
+            except AssertionError as ex:
+                raise RedisAggregatorAPIException("API registration process timed out.") from ex
+        return None
 
-    def _on_event_or_response(self, message):
+    def _on_event_or_response(self, message: Dict) -> None:
         log_msg = copy(message)
         log_msg.pop("grid_tree", None)
         logging.debug("A new message was received. Message information: %s", log_msg)
@@ -225,16 +232,19 @@ class RedisAggregator:
                              function_name="on_event_or_response")
 
     def calculate_grid_fee(self, start_market_or_device_name: str,
-                           target_market_or_device_name: str = None,
-                           fee_type: str = "current_market_fee"):
+                           target_market_or_device_name: Optional[str] = None,
+                           fee_type: str = "current_market_fee") -> Optional[float]:
+        """Calculate accumulated grid_fee of path between start_market_or_device_name
+        and target_market_or_device_name"""
         return self.grid_fee_calculation.calculate_grid_fee(start_market_or_device_name,
                                                             target_market_or_device_name, fee_type)
 
-    def get_uuid_from_area_name(self, name):
+    def get_uuid_from_area_name(self, name: str) -> str:
+        """Return area uuid from area name."""
         return get_uuid_from_area_name_in_tree_dict(self.area_name_uuid_mapping, name)
 
     @buffer_grid_tree_info
-    def _on_market_cycle(self, message):
+    def _on_market_cycle(self, message: Dict) -> None:
         self.area_name_uuid_mapping = \
             create_area_name_uuid_mapping_from_tree_info(self.latest_grid_tree_flat)
         self.grid_fee_calculation.handle_grid_stats(self.latest_grid_tree)
@@ -242,7 +252,7 @@ class RedisAggregator:
                              function_name="on_market_cycle")
 
     @buffer_grid_tree_info
-    def _on_tick(self, message):
+    def _on_tick(self, message: Dict) -> None:
         slot_completion_int = get_slot_completion_percentage_int_from_message(message)
         if slot_completion_int is not None and slot_completion_int < \
                 MIN_SLOT_COMPLETION_TICK_TRIGGER_PERCENTAGE:
@@ -251,27 +261,27 @@ class RedisAggregator:
                              function_name="on_tick")
 
     @buffer_grid_tree_info
-    def _on_trade(self, message):
+    def _on_trade(self, message: Dict) -> None:
         for individual_trade in message["trade_list"]:
             log_trade_info(individual_trade)
         self.executor.submit(execute_function_util, function=lambda: self.on_trade(message),
                              function_name="on_trade")
 
-    def _on_finish(self, message):
+    def _on_finish(self, message: Dict) -> None:
         self.executor.submit(execute_function_util, function=lambda: self.on_finish(message),
                              function_name="on_finish")
 
     def on_market_cycle(self, market_info):
-        pass
+        """Perform actions that should be triggered on market_cycle event."""
 
     def on_tick(self, tick_info):
-        pass
+        """Perform actions that should be triggered on tick event."""
 
     def on_trade(self, trade_info):
-        pass
+        """Perform actions that should be triggered on on_trade event."""
 
     def on_finish(self, finish_info):
-        pass
+        """Perform actions that should be triggered on on_finish event."""
 
     def on_event_or_response(self, message):
-        pass
+        """Perform actions that should be triggered on every event."""
