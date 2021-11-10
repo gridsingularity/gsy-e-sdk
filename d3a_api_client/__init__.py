545f67176e6c895fca986ac7a4364ce1f70ccd6e
faizan2590 <faizan2590@gmail.com>
2021-11-10 16:55:41 +0100

    D3ASIM-3669: Backward compatibility with d3a_api_client

---
+++ d3a_api_client/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+import gsy_e_sdk
+
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+logger.addHandler(console_handler)
+
+logger.warning(
+    "The d3a_api_client module name will be deprecated soon. Please use gsy_e_sdk instead.")
+
+__all__ = ["gsy_e_sdk"]
+
+sys.modules[__name__] = sys.modules["gsy_e_sdk"]
