ec557c17714c2a2fafa43300bb29c6a8774eb409
andreabertolini1995 <andre.berto95@gmail.com>
2022-04-26 12:53:17 +0200

    Proposed a script that would show how to send live data to a running Canary Network on the UI.

--- gsy_e_sdk/setups/Asset_API_scripts/canary_network_live_data.py
+++ gsy_e_sdk/setups/Asset_API_scripts/canary_network_live_data.py
@@ -25,35 +25,42 @@ class Oracle(aggregator_client_type):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.is_finished = False
-        self.base_forecast_load = 10
-        self.base_forecast_pv = 5
 
     def on_market_cycle(self, market_info):
         """Place a bid or an offer whenever a new market is created."""
         if self.is_finished is True:
             return
-        self.send_live_forecasts(market_info)
+        self.send_forecasts(market_info)
         self.execute_batch_commands()
 
     def on_tick(self, tick_info):
         pass
 
-    def send_live_forecasts(self, market_info):
-        """Send live forecasts to the exchange."""
-        for asset_name, asset in assets_dict.items():
-            forecast_market_slot = (
-                from_format(market_info["market_slot"], DATE_TIME_FORMAT)
-                .add(minutes=15)
-                .format(DATE_TIME_FORMAT)
-            )
-            if "Load" in asset_name:
-                energy_forecast = self.base_forecast_load
-            if "PV" in asset_name:
-                energy_forecast = self.base_forecast_pv
-            asset.set_energy_forecast(
-                energy_forecast_kWh={forecast_market_slot: energy_forecast},
-                do_not_wait=False,
-            )
+    def send_forecasts(self, market_info):
+        """Send forecasts of the next market slot to the exchange."""
+        forecast_market_slot = (
+            from_format(market_info["market_slot"], DATE_TIME_FORMAT)
+            .add(minutes=15)
+            .format(DATE_TIME_FORMAT)
+        )
+        # pylint: disable=unused-variable
+        for area_uuid, area_dict in self.latest_grid_tree_flat.items():
+            if "asset_info" not in area_dict or area_dict["asset_info"] is None:
+                continue
+            # Consumption assets
+            if "energy_requirement_kWh" in area_dict["asset_info"]:
+                asset_name = area_dict["area_name"]
+                globals()[f"{asset_name}"].set_energy_forecast(
+                    energy_forecast_kWh={forecast_market_slot: 1.2},
+                    do_not_wait=False,
+                )
+            # Generation assets
+            if "available_energy_kWh" in area_dict["asset_info"]:
+                asset_name = area_dict["area_name"]
+                globals()[f"{asset_name}"].set_energy_forecast(
+                    energy_forecast_kWh={forecast_market_slot: 0.86},
+                    do_not_wait=False,
+                )
 
     def on_event_or_response(self, message):
         pass
@@ -96,7 +103,6 @@ AssetClient = RestAssetClient
 simulation_id = os.environ["API_CLIENT_SIMULATION_ID"]
 domain_name = os.environ["API_CLIENT_DOMAIN_NAME"]
 websockets_domain_name = os.environ["API_CLIENT_WEBSOCKET_DOMAIN_NAME"]
-# asset_args = {"autoregister": False, "start_websocket": False}
 asset_args = {}
 if AUTOMATIC:
     registry = aggr.get_configuration_registry()
@@ -106,33 +112,27 @@ if AUTOMATIC:
     storage_names = registered_assets["Storage"]
 
 
-def register_asset_list(asset_names, asset_params, asset_uuid_map, asset_dict):
+def register_asset_list(asset_names, asset_params, asset_uuid_map):
     """Register the provided list of assets with the aggregator."""
     for asset_name in asset_names:
+        print("Registered asset:", asset_name)
         uuid = get_area_uuid_from_area_name_and_collaboration_id(
             simulation_id, asset_name, domain_name
         )
         asset_params["asset_uuid"] = uuid
         asset_uuid_map[uuid] = asset_name
-        asset = AssetClient(**asset_params)
-        asset.select_aggregator(aggr.aggregator_uuid)
-        asset_dict[asset_name] = asset
-    return asset_uuid_map, asset_dict
+        globals()[f"{asset_name}"] = AssetClient(**asset_params)
+        globals()[f"{asset_name}"].select_aggregator(aggr.aggregator_uuid)
+    return asset_uuid_map
 
 
 print()
 print("Registering assets ...")
 asset_uuid_mapping = {}
 assets_dict = {}
-asset_uuid_mapping, assets_dict = register_asset_list(
-    load_names, asset_args, asset_uuid_mapping, assets_dict
-)
-asset_uuid_mapping, assets_dict = register_asset_list(
-    pv_names, asset_args, asset_uuid_mapping, assets_dict
-)
-asset_uuid_mapping, assets_dict = register_asset_list(
-    storage_names, asset_args, asset_uuid_mapping, assets_dict
-)
+asset_uuid_mapping = register_asset_list(load_names, asset_args, asset_uuid_mapping)
+asset_uuid_mapping = register_asset_list(pv_names, asset_args, asset_uuid_mapping)
+asset_uuid_mapping = register_asset_list(storage_names, asset_args, asset_uuid_mapping)
 
 
 print()
